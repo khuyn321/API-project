f715435744fba35d79c7383d938001b67ac708b5
Kayla Huynh <khuyn321@gmail.com>
2022-10-26 21:17:40 -0700

    fixed api endpoint get-all-spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -37,67 +37,30 @@ router.use(express.json())
 //* then I want to average those reviews (sumOfReviews / revCount)
 //* and res.json an "avgRating" with that calculated avg
 
-router.get('/', async (req, res) => {
-  // grab all needed information
-  const spots = await Spot.findAll(
-    {
+router.get('/', async (_req, res, _next) => {
+  const allSpots = await Spot.findAll({
+    include: [
+      { model: Review, attributes: [] },
+      { model: SpotImage, attributes: [] }
+    ],
+    attributes: {
       include: [
-        {
-          model: Review
-        },
-        {
-          model: SpotImage
-        },
-      ],
-    }
-  )
-
-  //create  an empty arr to store spots.toJSON-ed
-  const spotList = []
-
-  spots.forEach(async spot => {
-    spotList.push(spot.toJSON())
-  })
-
-  //for each spot,
-  spotList.forEach(async spot => {
-    let reviewArr = spot.Reviews
-    let sumRatings = 0
-
-    // grab the sum of all ratings
-    reviewArr.forEach(review => {
-      sumRatings += review.stars
-    })
-
-    // find avg of the sum of ratings
-    let avgRating = sumRatings / reviewArr.length
-    spot.avgRating = avgRating
-
-    // GET RID OF Reviews info now that keyVal is set for avgRating
-    delete spot.Reviews
-
-    let spotImgArr = spot.SpotImages
-
-    spotImgArr.forEach(async image => {
-      if (image.preview === true) {
-        spot.previewImage = image.url
-      }
-      delete spot.SpotImages
-    })
-
+        [Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 'avgRating'],
+        [Sequelize.col('SpotImages.url'), 'previewImage']
+      ]
+    },
+    group: ['Spot.id']
   });
 
-
-
-  return res.json({
-    Spots: spotList
-  })
-})
+  return res.json({ Spots: allSpots });
+});
 
 
 module.exports = router;
 
-//todo)   unused/flawed code v v v
+
+
+//todo)   scrapped/flawed code v v v
 
       // attributes: {
       //   include: [
@@ -127,3 +90,60 @@ module.exports = router;
 //     spot.SpotImages[0].preview = previewUrl
 //   }
 // });
+
+// router.get('/', async (req, res) => {
+//   // grab all needed information
+//   const spots = await Spot.findAll(
+//     {
+//       include: [
+//         {
+//           model: Review
+//         },
+//         {
+//           model: SpotImage
+//         },
+//       ],
+//     }
+//   )
+
+//   //create  an empty arr to store spots.toJSON-ed
+//   const spotList = []
+
+//   spots.forEach(async spot => {
+//     spotList.push(spot.toJSON())
+//   })
+
+//   //for each spot,
+//   spotList.forEach(async spot => {
+//     let reviewArr = spot.Reviews
+//     let sumRatings = 0
+
+//     // grab the sum of all ratings
+//     reviewArr.forEach(review => {
+//       sumRatings += review.stars
+//     })
+
+//     // find avg of the sum of ratings
+//     let avgRating = sumRatings / reviewArr.length
+//     spot.avgRating = avgRating
+
+//     // GET RID OF Reviews info now that keyVal is set for avgRating
+//     delete spot.Reviews
+
+//     let spotImgArr = spot.SpotImages
+
+//     spotImgArr.forEach(async image => {
+//       if (image.preview === true) {
+//         spot.previewImage = image.url
+//       }
+//       delete spot.SpotImages
+//     })
+
+//   });
+
+
+
+//   return res.json({
+//     Spots: spotList
+//   })
+// })
