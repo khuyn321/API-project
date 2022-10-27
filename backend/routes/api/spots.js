282ecabd586a4132d69941a19b81629d245720c8
Kayla Huynh <khuyn321@gmail.com>
2022-10-27 03:10:12 -0700

    added get-spots-of-current-user endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -43,6 +43,61 @@ const validateNewSpot = [
   handleValidationErrors
 ];
 
+//!              GET SPOTS OF CURRENT USER
+
+router.get('/current', async (req, res) => {
+  const { user } = req
+  const allSpots = await Spot.findAll({
+    where: {
+      ownerId: user.id
+    }
+  })
+
+  const payload = [];
+  for (let i = 0; i < allSpots.length; i++) { //lazy loading to avoid conflicts w/ Postgres
+    const spot = allSpots[i]
+
+    const review = await spot.getReviews({  //aggregate function to find average of Stars column
+      attributes: [
+        [Sequelize.fn('AVG', Sequelize.col('stars')), 'avgRating']
+      ]
+    })
+
+    const avgRating = review[0].toJSON().avgRating //keying to grab the value
+
+    let spotImage = await SpotImage.findOne({      //finds the first image that has a truthy preview
+      where: {
+        preview: true,
+        spotId: spot.id
+      }
+    })
+
+    spotImage ? spotImage = spotImage.url : null
+
+    const spotData = {
+      id: spot.id,
+      ownerId: spot.ownerId,
+      address: spot.address,
+      city: spot.city,
+      state: spot.state,
+      country: spot.country,
+      lat: spot.lat,
+      lng: spot.lng,
+      name: spot.name,
+      description: spot.description,
+      price: spot.price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+      avgRating: avgRating,
+      previewImage: spotImage
+    }
+    payload.push(spotData)
+  }
+  res.json({
+    Spots: payload
+  })
+})
+
 //!              CREATE A NEW SPOT
 
 router.post(
