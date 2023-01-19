48021bc90b695f9729eb1900c1290138104be91f
Kayla Huynh <khuyn321@gmail.com>
2023-01-19 00:46:05 -0800

    completed signup Form page

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,8 +1,8 @@
-// frontend/src/components/SignupFormPage/index.js
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import './SignupForm.css';
 
 function SignupFormPage() {
   const dispatch = useDispatch();
