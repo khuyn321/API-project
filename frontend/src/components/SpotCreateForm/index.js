83002bf866e47e35385defe99b74c80925397b27
Kayla Huynh <khuyn321@gmail.com>
2023-03-14 15:03:50 -0700

    very simple spotCreateForm css

--- frontend/src/components/SpotCreateForm/index.js
+++ frontend/src/components/SpotCreateForm/index.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useHistory } from "react-router-dom"
 import { createSpot } from "../../store/spots"
+import './SpotCreateForm.css';
 
 function formValidator(name, description, price, address, city, state, country, image) {
   const errors = []
